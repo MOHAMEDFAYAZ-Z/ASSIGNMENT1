a28b7fca5f35e8400877a090d8917b5620175f60
MOHAMEDFAYAZ-Z <mohamedfayaz.2205054@srec.ac.in>
2026-06-05 17:13:27 +0530

    Add files via upload

---
+++ ASSIGNMENT/AssignmentMain2/AppointmentBusinessLogic.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public partial class Appointment
+{
+    public void Display()
+    {
+        Console.WriteLine($"{AppointmentId} {PatientName} {Department} {Status} {AppointmentDate.ToShortDateString()} {ConsultationFee}");
+    }
+}
