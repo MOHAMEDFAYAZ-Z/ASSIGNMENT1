a97fd08d6c31594336aa380fa9cec483bafebc9b
MOHAMEDFAYAZ-Z <mohamedfayaz.2205054@srec.ac.in>
2026-06-08 09:28:14 +0530

    Delete ASSIGNMENT/AssignmentMain2 directory

--- ASSIGNMENT/AssignmentMain2/AppointmentBusinessLogic.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public partial class Appointment
-{
-    public void Display()
-    {
-        Console.WriteLine($"{AppointmentId} {PatientName} {Department} {Status} {AppointmentDate.ToShortDateString()} {ConsultationFee}");
-    }
-}
