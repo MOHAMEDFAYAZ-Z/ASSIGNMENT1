0181e9936eb8e99b82e436a8e0498dd6a57ad6bb
MOHAMEDFAYAZ-Z <mohamedfayaz.2205054@srec.ac.in>
2026-06-08 12:59:54 +0530

    Add files via upload

---
+++ Assignment - Console Based Appointment App(SOLID Principle)/AppointmentValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public partial class Appointment
+{
+    public bool Validate()
+    {
+        return !string.IsNullOrEmpty(PatientName) &&
+               !string.IsNullOrEmpty(Department) &&
+               ConsultationFee > 0;
+    }
+}
